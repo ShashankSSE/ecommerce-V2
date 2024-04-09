ccac5745ff33166e8514dc4c427fd5edfb7fd1c5
ShashankSSE <shashank@xenelsoft.co.in>
2024-04-09 22:04:15 +0530

    pages creation and some homepage fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Models\Settings;
 use App\Models\Category;
 use App\Models\Cart;
 use App\Models\Banner;
+use App\Models\Pages;
 use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
@@ -33,11 +34,18 @@ class AppServiceProvider extends ServiceProvider
         $cart = Cart::where('is_purchased','=',0)->get();
         $totalProductInCart = count($cart);
         $banners = Banner::where('status','=',1)->orderBy('sequence','asc')->get(); 
+        $pages = Pages::select('title','slug')->where('is_active','=',1)->get();
+        $footerLinks = null;
+        if(count($pages)>0){
+            $footerLinks = $pages;
+        }
+        
         View::share([
             'settings' => $settings,
             'categories' => $categories,
             'totalProductInCart' => $totalProductInCart,
             'banners' => $banners,
+            'footerLinks' => $footerLinks,
         ]);
     }
 }
---
+++ resources/views/pages/faq.blade.php
@@ -0,0 +1,42 @@
+@extends('components.app')
+@section('title', 'Patakah')
+@section('meta_description', '')
+@section('meta_keywords','')
+@section('content')
+    <div class="container faq">
+        <div class="heading">
+            Faq's
+        </div>
+        <div class="accordion" id="accordionExample">
+            @if(count($faqs)>0)
+            @foreach($faqs as $faq) 
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="faq_{{$faq->id}}">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapsefaq_{{$faq->id}}" aria-expanded="true" aria-controls="collapsefaq_{{$faq->id}}">
+                    {{$faq->question}}
+                </button>
+                </h2>
+                <div id="collapsefaq_{{$faq->id}}" class="accordion-collapse collapse show" aria-labelledby="faq_{{$faq->id}}" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        {!!$faq->answere!!}
+                    </div>
+                </div>
+            </div> 
+            @endforeach
+            @else
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="faq">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapsefaq" aria-expanded="true" aria-controls="collapsefaq">
+                    No FAQ's are available for now.
+                </button>
+                </h2>
+                <div id="collapsefaq" class="accordion-collapse collapse show" aria-labelledby="faq" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        No FAQ's are available for now.
+                    </div>
+                </div>
+            </div> 
+            @endif
+        </div>
+    </div>
+@endsection
